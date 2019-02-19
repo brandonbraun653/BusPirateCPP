0845b539bff0d9fa0e1fc433021a65e8b88c4477
Brandon Braun <brandonbraun653@gmail.com>
2019-02-19 17:20:03 -0600

    Project update. Submodule update

--- src/serial_driver.cpp
+++ src/serial_driver.cpp
@@ -1,11 +1,56 @@
 /********************************************************************************
-*   File Name:
-*       serial_driver.cpp
-*
-*   Description:
-*       Implements the interface to the host PC's serial port
-*
-*   2019 | Brandon Braun | brandonbraun653@gmail.com
-********************************************************************************/
+ *   File Name:
+ *       serial_driver.cpp
+ *
+ *   Description:
+ *       Implements the interface to the host PC's serial port
+ *
+ *   2019 | Brandon Braun | brandonbraun653@gmail.com
+ ********************************************************************************/
 
+/* Module Includes */
 #include "serial_driver.hpp"
+
+
+
+
+SerialDriver::SerialDriver(std::string &comPort)
+{
+
+}
+
+Chimera::Serial::Status SerialDriver::begin( const uint32_t baud, const Chimera::Serial::Modes txMode,
+                                             const Chimera::Serial::Modes rxMode )
+{
+  throw std::logic_error( "The method or operation is not implemented." );
+}
+
+Chimera::Serial::Status SerialDriver::setBaud( const uint32_t buad )
+{
+  throw std::logic_error( "The method or operation is not implemented." );
+}
+
+Chimera::Serial::Status SerialDriver::setMode( const Chimera::Serial::SubPeripheral periph, const Chimera::Serial::Modes mode )
+{
+  throw std::logic_error( "The method or operation is not implemented." );
+}
+
+Chimera::Serial::Status SerialDriver::write( const uint8_t *const buffer, const size_t length, const uint32_t timeout_mS )
+{
+  throw std::logic_error( "The method or operation is not implemented." );
+}
+
+Chimera::Serial::Status SerialDriver::read( uint8_t *const buffer, const size_t length, const uint32_t timeout_mS )
+{
+  throw std::logic_error( "The method or operation is not implemented." );
+}
+
+bool SerialDriver::reserve( const uint32_t timeout_mS )
+{
+  throw std::logic_error( "The method or operation is not implemented." );
+}
+
+bool SerialDriver::release( const uint32_t timeout_mS )
+{
+  throw std::logic_error( "The method or operation is not implemented." );
+}
--- src/serial_driver.hpp
+++ src/serial_driver.hpp
@@ -1,17 +1,48 @@
 /********************************************************************************
-*   File Name:
-*     serial_driver.hpp
-*
-*   Description:
-*     Provides an interface to the host computer's serial port.
-*
-*   2019 | Brandon Braun | brandonbraun653@gmail.com
-********************************************************************************/
+ *   File Name:
+ *     serial_driver.hpp
+ *
+ *   Description:
+ *     Provides an interface to the host computer's serial port.
+ *
+ *   2019 | Brandon Braun | brandonbraun653@gmail.com
+ ********************************************************************************/
 #pragma once
 #ifndef BUS_PIRATE_CPP_SERIAL_DRIVER_HPP
 #define BUS_PIRATE_CPP_SERIAL_DRIVER_HPP
 
+/* C++ Includes */
+#include <string>
 
+/* Boost Includes */
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/asio/serial_port.hpp>
+
+/* Chimera Includes */
+#include <Chimera/interface.hpp>
+
+class SerialDriver : public Chimera::Serial::Interface
+{
+public:
+  SerialDriver(std::string &comPort);
+  ~SerialDriver() = default;
+
+  Chimera::Serial::Status begin( const uint32_t baud, const Chimera::Serial::Modes txMode,
+                                 const Chimera::Serial::Modes rxMode ) override;
+
+  Chimera::Serial::Status setBaud( const uint32_t buad ) override;
+
+  Chimera::Serial::Status setMode( const Chimera::Serial::SubPeripheral periph, const Chimera::Serial::Modes mode ) override;
+
+  Chimera::Serial::Status write( const uint8_t *const buffer, const size_t length, const uint32_t timeout_mS = 500 ) override;
+
+  Chimera::Serial::Status read( uint8_t *const buffer, const size_t length, const uint32_t timeout_mS = 500 ) override;
+
+  bool reserve( const uint32_t timeout_mS ) override;
+
+  bool release( const uint32_t timeout_mS ) override;
+};
 
 
 #endif /* !BUS_PIRATE_CPP_SERIAL_DRIVER_HPP */
